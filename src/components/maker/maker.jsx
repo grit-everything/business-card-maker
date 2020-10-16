ddab14a17a69fd81cd086812c3d652bf3fa6523c
jake_lyu <gomgom3409@gmail.com>
2020-10-16 21:44:39 +1100

    add editor & preview layout

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -4,6 +4,8 @@ import { useHistory } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './maker.module.css';
+import Editor from '../editor/editor';
+import Preview from '../preview/preview';
 const Maker = ({ authService }) => {
   const history = useHistory();
   const onLogout = () => {
@@ -21,6 +23,11 @@ const Maker = ({ authService }) => {
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor />
+        <Preview />
+      </div>
+
       <Footer />
     </section>
   );
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -12,7 +12,10 @@ class AuthService {
   }
 
   onAuthChange(onUserChanged) {
+    // onAuthCahnge 함수는 인자로 onUserChanged라는 콜백함수를 받고 login logout이 발생하면 onAuthStateChanged로 부터 변경된 유저 정보를 인자로 받는다.
     firebase.auth().onAuthStateChanged((user) => {
+      //onAuthStateChanged 함수는 콜백 함수를 받고, 사용자의 로그인 상태를 감시한다. 로그인 로그아웃 할 때마다 콜백함수가 호출된다.
+
       onUserChanged(user);
     });
   }
