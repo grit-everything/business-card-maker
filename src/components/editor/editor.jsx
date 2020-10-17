9ec132281ab5a8bb3fc36a5869cce47400b7e83b
jake_lyu <gomgom3409@gmail.com>
2020-10-17 16:09:50 +1100

    add card and edit from component

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -3,7 +3,7 @@ import CardEditForm from '../card_edit_form/card_edit_form';
 import styles from './editor.module.css';
 const Editor = ({ cards }) => (
   <section className={styles.editor}>
-    <h1 className={styles.title}>Card Maker</h1>;
+    <h1 className={styles.title}>Card Maker</h1>
     {cards.map((card) => (
       <CardEditForm card={card} key={card.id} />
     ))}
