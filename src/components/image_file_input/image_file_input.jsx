cadf32a28ca57e4516ad3a6fcdeb8aadb05084ad
jake_lyu <gomgom3409@gmail.com>
2020-10-22 05:52:22 +1100

    add loading spin

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useState } from 'react';
 import { useRef } from 'react';
 import styles from './image_file_input.module.css';
 
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+  const [loading, setLoading] = useState(false);
+
   const inputRef = useRef();
   const onButtonClick = (e) => {
     e.preventDefault();
@@ -10,12 +13,13 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
     inputRef.current.click();
   };
   const onChange = async (e) => {
+    setLoading(true);
     // imageUploader.upload(e.target.files[0]).then(console.log);
     //=> ImageUploader가 async이기 떄문에 promise를 리턴하고, then then tehn 해서 처리된 값을 사용 또는. e 자체에 async를 해주고 await 를 사용해도 된다
     console.log(e.target.files[0]);
     console.log(e.target);
     const uploaded = await imageUploader.upload(e.target.files[0]);
-    console.log(uploaded);
+    setLoading(false);
     onFileChange({
       name: uploaded.original_filename,
       url: uploaded.url,
@@ -32,12 +36,16 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
         name="file"
         onChange={onChange}
       />
-      <button
-        className={styles.button} //
-        onClick={onButtonClick}
-      >
-        {name || 'No file'}
-      </button>
+      {!loading && (
+        <button
+          className={`${styles.button} ${name ? styles.pink : styles.grey}`} //
+          onClick={onButtonClick}
+        >
+          {name || 'No file'}
+        </button>
+      )}
+
+      {loading && <div className={styles.loading}></div>}
     </div>
   );
 };
